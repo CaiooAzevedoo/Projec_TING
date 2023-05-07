e8fd0fe97721ce93b3a47a34ccbdf29f682b994d
Caio <caiusito@gmail.com>
2023-05-07 10:50:06 -0300

    Feat: Req 2.3

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -2,10 +2,11 @@ import sys
 
 
 def txt_importer(path_file):
-    txt_file = open(path_file, 'r')
-    while txt_file:
-        if path_file[-1:-4] != '.txt' and txt_file is not None:
-            print('Formato inválido', file=sys.stderr)
-        else:
-            raise FileNotFoundError
-        return txt_file.read().split(sep='\n')
+    try:
+        txt_file = open(path_file, 'r')
+        while txt_file:
+            if path_file[-1:-4] != '.txt' and txt_file is not None:
+                print('Formato inválido', file=sys.stderr)
+            return txt_file.read().split(sep='\n')
+    except FileNotFoundError:
+        print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
