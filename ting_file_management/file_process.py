b6407e1999b4c68aab824ef0800141aa31f76234
Caio <caiusito@gmail.com>
2023-05-07 10:59:58 -0300

    Feat: Req 3 - process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,18 @@
+from .file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    lines = txt_importer(path_file)
+    item = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines
+    }
+    if item not in instance.queue:
+        instance.enqueue(item)
+
+    print(item, file=sys.stdout)
 
 
 def remove(instance):
